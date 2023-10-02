2845fcbe3c1267d2e355ae7ec8ae04c9e405516b
Wambuku <julietwambuku361@gmail.com>
2023-10-02 11:50:29 +0300

    Signupform

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<SignupComponent />
+</template>
+
+
+<script>
+import SignupComponent from "./components/SignupComponent.vue";
+export default {
+  name: "App",
+  components: { SignupComponent },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: grey;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
---
+++ src/components/jobs/Jobs.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id" class="job">
+    <router-link :to="{name: 'JobDetails', params: { id: job.id}}">
+    <h2>{{ job.title }}</h2>
+    </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+        {"title": "Ninja UX Designer", "id": 1, "details": "lorem"},
+        {"title": "Ninja Web developer", "id": 2, "details": "lorem"},
+        {"title": "Ninja Vue Developer", "id": 3, "details": "lorem"}
+         ]
+        }
+    }
+}
+</script>
+<style>
+.job h2{
+    background: white;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 10px auto;
+}
+</style>
